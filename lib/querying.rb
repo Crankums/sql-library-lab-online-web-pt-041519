71e425011e75e23ba68df01660ed68fe86c0cd1c
Craig Donavin <crdonavin@gmail.com>
2019-06-24 19:12:35 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,28 +1,25 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year FROM books WHERE series_id = 1 ORDER BY books.year ASC"
+  "Write your SQL query here"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT characters.name, characters.motto FROM characters ORDER BY LENGTH(motto) DESC LIMIT 1"
+  "Write your SQL query here"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(*) FROM characters GROUP BY species ORDER by COUNT(*) DESC LIMIT 1"
+  "Write your SQL query here"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name
-    FROM authors
-    JOIN series
-    ON series.author_id = authors.id
-    JOIN subgenres
-    ON series.subgenre_id = subgenres.id"
-
+  "Write your SQL query here"
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series JOIN characters ON series.id = characters.series_id"
+  "SELECT series.title
+   FROM series
+   INNER JOIN characters
+   ON series.id = characters.series_id"
 end
 
 def select_character_names_and_number_of_books_they_are_in
